176cea545f61e78a347c0104d4aab1c888061299
Einar W. Høst <einarwh@gmail.com>
2017-05-11 22:44:14 +0200

    next-link

--- Hyperlamb/Hyperlamb.fs
+++ Hyperlamb/Hyperlamb.fs
@@ -13,6 +13,36 @@ open Types
 
 open Eval
 
+let rec listBoundVariables exp =
+  match exp with
+  | Var v -> []
+  | Lam (v, e) ->
+    v :: listBoundVariables e
+  | App (e1, e2) ->
+    listBoundVariables e1 @ listBoundVariables e2
+
+let toTokenString tokens = 
+  let t2s t = 
+    match t with 
+    | Reference n -> sprintf "R%d" n
+    | Abstraction -> "L"
+    | Application -> "A"
+  let strs = tokens |> List.map t2s
+  String.concat "" strs
+
+let rec tokenify scope exp : Token list = 
+  match exp with
+  | Var v -> 
+    let index = scope |> List.findIndex (fun it -> it = v)
+    [ Reference (index + 1) ]
+  | Lam (v, e) ->
+    let tokens = tokenify (v :: scope) e 
+    Abstraction :: tokens
+  | App (e1, e2) ->
+    let tokens1 = tokenify scope e1
+    let tokens2 = tokenify scope e2
+    Application :: (tokens1 @ tokens2)
+
 let unparse =
     let pstr s = "(" + s + ")"
     let rec unparse = function
@@ -190,10 +220,19 @@ let createTextPlainResponse maybeExpResult =
   | None ->
     "nope" |> OK
 
+let toQueryString (bounds : string list) = 
+  if bounds.Length = 0 then ""
+  else bounds |> String.concat "&" |> sprintf "?%s"
+
 let createTextHtmlResponse maybeExpResult =
   match maybeExpResult with 
   | Some { self = exp; next = Some exp' } ->
-    sprintf "<html><meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\"/><body><p>%s</p><a href=\"%s\">Next</a></body></html>" (unparse exp) (unparse exp') |> OK
+    let scope = []
+    let tokenString = tokenify scope exp' |> toTokenString 
+    let bounds = listBoundVariables exp'
+    let nextLink = tokenString + toQueryString bounds
+    printfn "%A" bounds
+    sprintf "<html><meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\"/><body><p>%s</p><a href=\"%s\">Next</a></body></html>" (unparse exp) nextLink |> OK
   | Some { self = exp; next = None } ->
     let response = (sprintf "<html><meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\"/><body><p>%s</p></body>" (unparse exp) |> OK)
     response >=> Writers.setMimeType "text/html"
